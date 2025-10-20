2b0dccde9a04a4181515513aeb3777bf7a1cde4a
Injills223 <S22310335@student.unklab.ac.id>
2025-10-20 23:31:30 +0800

    semua latihan

--- pertemuan-11/index.js
+++ pertemuan-11/index.js
@@ -1,60 +1,96 @@
-//Rest Parameter & Spread Operator
-//1.Rest Parameter 
+//Destructuring & Module
+//1. Destructuring Array & Object
 
-//function tanpa rest parameter 
-// const funct1 = (param1, param2, param3, param4, param5) => {
-//     const arr = [param1, param2, param3, param4, param5];
-//     let hasil = 0;
-//     arr.forEach((item) => (hasil += item));
-//     console.log(hasil);
-// }; 
+//Structuring Array/membuat array
+// let numbers = [1, 2, 3, 4, 5];
 
-// funct1(1, 2, 3, 4, 5);  
+//Destructuring Array
+// let num1 = numbers[0];
+// let num2 = numbers[1];
+// let num3 = numbers[2];
+// let num4 = numbers[3];
+// let num5 = numbers[4];
 
-//dengan rest parameter 
-//a. rest paramter bertipe array 
-//b. rest paramter harus ada di terakhir dari paramter list 
-// const funct1 = (param1, param2, ...rest) => {
-//     let hasil = 0; 
-//     rest.forEach((item) => (hasil += item)); 
-//     console.log(hasil); 
-// } 
+// const [num1, num2, num3, num4, num5] = numbers;
+// console.log(num1, num2, num3, num4, num5);
 
-// funct1(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);  
+//Destructuring sebagaian item dlm array
+//dilihat dari posisi index
+// const [, num2, num3, ,] = numbers;
+// console.log(num2, num3);
 
-//2. Spread Operator 
-// const numbers = [1, 2, 3, 4, 5]; 
-// console.log(numbers); 
-// console.log(...numbers); 
+//Destructuring dengan rest parameter
+// const [num1, ...rest] = numbers;
+// console.log(num1, rest);
 
-//Kegunaan Spread Operator pada Array 
-//1.Duplikat array 
-// const numbers2 = [...numbers];
-// console.log(numbers2);
-
-//2.Menggabungkan array 
-// let arr1 = [1, 2, 3]; 
-// let arr2 = [4, 5, 6]; 
-// let arr3 = [7, 8, 9]; 
-
-// let combineArr1 = arr1.concat(arr2).concat(arr3); 
-// console.log(combineArr1)
-// let combineArr2= [...arr1, ...arr2, ...arr3]; 
-// console.log(combineArr2); 
-
-//Kegunaan Spread Operator pada Objec 
-//1.Duplikasi Object 
+//Destructuring Object
 // const student1 = {
-//     firstName: "John",
-//     status: "active", 
-// }; 
-// console.log(student1); 
-// const student2 = { ...student1, address: "Manado" }; 
-// console.log(student2); 
+//   fullName: "John",
+//   age: 25,
+//   status: "active",
+// };
 
-//2.Menggabungkan Object 
-// const obj1 = { a: 1, b: 2 };
-// const obj2 = { a: 3, b: 4 }; 
+// const fullName = student1.fullName;
+// const age = student1.age;
+// const status = student1.status;
 
-// const combineObj = { ...obj1, ...obj2 }; 
-// console.log(combineObj);  
+// const { fullName, age, status } = student1;
+// console.log(fullName, age, status);
+
+//Destructuring Sebagian item
+// const { age } = student1;
+// console.log(age);
+
+//Destructuring object dengan rest
+//Tipe data rest parameter pada object adalah bertipe object
+// const { fullName, ...rest } = student1;
+// console.log(fullName);
+// console.log(rest);
+
+//Mini Exercise
+//Lihat exercise 4 modifikasi dibagian yang dapat dibuat
+// distructuring array/object
+
+// function calculateAge(birthYear) {
+//   return 2019 - birthYear;
+// }
+
+// function yearUntilRetirement({ year, firstName }) {
+//   var age = calculateAge(year);
+//   var retirement = 60 - age;
+
+//   if (retirement > 0) {
+//     console.log(firstName + " retires in " + retirement + " years");
+//   } else {
+//     console.log(firstName + " is already retired.");
+//   }
+// }
+
+// yearUntilRetirement({ year: 1987, firstName: "John" });
+
+// var phi = 3.14;
+// var power = 2;
+// var radius = 0;
+
+// var calculateArea = function ({ radius, power }) {
+//   return phi * Math.pow(radius, power);
+// };
+
+// radius = 21;
+// var area21 = calculateArea({ radius: radius, power: 2 });
+
+// radius = 7;
+// var area7 = calculateArea({ radius: radius, power: 2 });
+
+// console.log(
+//   "area with 21 radius: " + area21 + ", and area with 7 radius: " + area7
+// );
+
+//2. Module
+import { student as mahasiswa, angka } from "./data.js";
+import ucapkanSalam from "./sayGreetings.js";
+
+console.log(mahasiswa);
+console.log(angka);
+
+ucapkanSalam();
---
+++ pertemuan-12/index.js
@@ -0,0 +1,90 @@
+//Asynchronous JavaScript
+
+//Synchronous -> single thread
+
+// console.log("Proses 1");
+// console.log("Proses 2");
+// console.log("Proses 3");
+// console.log("Proses 4");
+
+//Asynchronous -> multi thread -> non blocking
+//1. parallel
+// setTimeout(() => {
+//   console.log("Proses 1");
+// }, 1000);
+// setTimeout(() => {
+//   console.log("Proses 2");
+// }, 2000);
+// setTimeout(() => {
+//   console.log("Proses 3");
+// }, 5000);
+// setTimeout(() => {
+//   console.log("Proses 4");
+// }, 4000);
+//2. concurrent
+// setTimeout(() => {
+//   console.log("Proses 1");
+//   setTimeout(() => {
+//     console.log("Proses 2");
+//     setTimeout(() => {
+//       console.log("Proses 3");
+//       setTimeout(() => {
+//         console.log("Proses 4");
+//         setTimeout(() => {
+//           console.log("Proses 3");
+//         }, 1000);
+//       }, 1000);
+//     }, 3000);
+//   }, 3000);
+// }, 3000);
+
+//Promise
+// let condition = True;
+// const newPromise = new Promise((resolve, reject) => {
+//   if (condition) {
+//     resolve("Berhasil");
+//   } else {
+//     reject("Gagal");
+//   }
+// });
+
+//pakai/consume Promise
+//1. then - catch
+// newPromise
+//   .then((result) => ${result} !!!) //Proses 1
+//   .then((result2) => console.log(result2)) //Proses 2
+//   .catch((error) => console.log(error));
+
+//2. async - await
+//Buat dalam function
+// (async () => {
+//   let result = await newPromise;
+//   console.log(result);
+// })();
+
+//Test get data API from JSONPlaceholder
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((res) => res.json())
+  .then((users) => console.log(users));
+
+//Mini Exercise
+//Ubah fetch menggunakan async-await tampilkan field name saja
+// const getData = async () => {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//   const users = await response.json();
+//   users.forEach((item) => console.log(item.name));
+// };
+
+// getData();
+
+(async () => {
+  const result = await fetch("https://reqres.in/api/users");
+  const json = await result.json();
+
+  console.log(json);
+})();
+
+console.log("Test conflict beda file");
+
+
+
